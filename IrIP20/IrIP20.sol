c280298da6df66b2562a38abdcee1f09d6474e3d
ldcc <ldcc@live.com>
2018-07-16 18:04:51 +0800

    update IrIP20

--- IrIP20/IrIP20.sol
+++ IrIP20/IrIP20.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.4.21;
 
 
 contract IrIP20Interface {
-    mapping(address => uint256) public balances;
-    mapping(address => mapping(address => uint256)) public allowed;
-    mapping(address => bool) public owners;
+
+    mapping(address => uint256) internal balances;
+    mapping(address => mapping(address => uint256)) internal allowed;
+    mapping(address => mapping(bool => bool)) internal licensees;
 
     /// total amount of tokens
     uint256 public totalSupply;
@@ -19,11 +20,22 @@ contract IrIP20Interface {
     /// @return Amount of remaining tokens allowed to spent
     function allowanceOf(address _owner, address _spender) public view returns (uint256 remaining);
 
+    /// @param _licensee The address of` the account is it licensed withdraw
+    /// @param _type The address of the account able to transfer the tokens
+    /// @return Authorization of licensed currency is it allowed to withdraw
+    function licenseOf(address _licensee, bool _type) public view returns (bool licensed);
+
     /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of tokens to be approved for transfer
     /// @return Whether the approval was successful or not
     function approve(address _spender, uint256 _value) public returns (bool success);
+
+    /// @notice `msg.sender` licensing `_licensee` to withdraw some `_type` of currency
+    /// @param _licensee The address of the account able to withdraw the tokens
+    /// @param _type The currency type to be licensing for transfer
+    /// @return Whether the licensing was successful or not
+    function licensing(address _licensee, bool _type) public returns (bool success);
 
     /// @notice send `_value` token to `_to` from `msg.sender`
     /// @param _to The address of the recipient
@@ -41,14 +53,15 @@ contract IrIP20Interface {
     /// @notice send `_values[i]` token to `_tos[i]` from `msg.sender`
     /// @param _tos The addresses of the recipient array
     /// @param _values The amounts of token to be transferred array
-    /// @return Whether the transfer was successful or not
+    /// @return Whether the transfers was successful or not
     function mulTransfer(address[] _tos, uint256[] _values) public returns (bool success);
 
     /// @notice withdraw `_value` Wei to `_to` from this contract`
     /// @param _to The address of the recipient
     /// @param _value The amount of Wei to be transferred
-    /// @return Whether the transfer was successful or not
-    function withdraw(address _to, uint256 _value) public returns (bool success);
+    /// @param _value The currency type of _to to be withdraw, true rep IRC, false rep the token of this contract
+    /// @return Whether the withdraw was successful or not
+    function withdraw(address _to, uint256 _value, bool _type) public payable returns (bool success);
 
     // solhint-disable-next-line no-simple-event-func-name
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
@@ -64,13 +77,15 @@ contract IrIP20 is IrIP20Interface {
 
     constructor(uint256 _initialAmount, string _name, string _symbol, uint8 _costpc) public payable {
         totalSupply = _initialAmount * 10 ** uint256(decimals);
-        balances[msg.sender] = _initialAmount;
         name = _name;
         symbol = _symbol;
         costpc = _costpc;
+        balances[msg.sender] = _initialAmount;
+        licensees[msg.sender][true] = true;
+        licensees[msg.sender][false] = true;
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance){
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
@@ -78,20 +93,37 @@ contract IrIP20 is IrIP20Interface {
         return allowed[_owner][_spender];
     }
 
-    function _transfer(address _from, address _to, uint256 _value) internal {
+    function licenseOf(address _licensee, bool _type) public view returns (bool licensed) {
+        return licensees[_licensee][_type];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function licensing(address _licensee, bool _type) public returns (bool success) {
+        require(licensees[msg.sender][_type]);
+        licensees[_licensee][_type] = true;
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) private {
         require(_value > 0);
         require(balances[_from] >= _value);
-        _deduction(_to, _value);
         balances[_from] -= _value;
         balances[_to] += _value;
+        _deduction(_to, _value);
         emit Transfer(_from, _to, _value);
     }
 
-    function _deduction(address _to, uint256 _value) internal {
+    function _deduction(address _to, uint256 _value) private {
         require(costpc <= 100);
         uint256 cost = _value * costpc / 100;
         require(_value > cost);
         balances[_to] -= cost;
+        balances[this] += cost;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
@@ -108,12 +140,6 @@ contract IrIP20 is IrIP20Interface {
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
     function mulTransfer(address[] _tos, uint256[] _values) public returns (bool success) {
         require(_tos.length == _values.length);
         uint256 i = 0;
@@ -124,9 +150,14 @@ contract IrIP20 is IrIP20Interface {
         return true;
     }
 
-    function withdraw(address _to, uint256 _value) public returns (bool success) {
-        require(owners[_to]);
-        return _to.send(_value);
+    function withdraw(address _to, uint256 _value, bool _type) public payable returns (bool success) {
+        require(licensees[msg.sender][_type]);
+        if (_type) {
+            _to.transfer(_value);
+        } else {
+            _transfer(this, _to, _value);
+        }
+        return true;
     }
 
 }

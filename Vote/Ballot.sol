dd383c9fea392f959228a6b14ce370458a3443db
ldcc <ldcc@live.com>
2018-07-17 20:11:38 +0800

    fix cast vote bugs, add ticket

--- Vote/Ballot.sol
+++ Vote/Ballot.sol
@@ -4,20 +4,20 @@ pragma solidity ^0.4.21;
 contract Ballot {
 
     struct Voter {
-        bool voted;
-        uint8 vote;
+        uint256 ticket;
+        mapping(uint8 => uint8) votes;
     }
 
     struct Proposal {
         uint8 vote;
-        bytes32 name;
+        bytes16 name;
         bytes32 desc;
         uint256 supporters;
     }
 
     mapping(address => Voter) public voters;
 
-    bool public state;
+    bool public status;
     string public name;
     address public author;
     uint256 public totalSupply;
@@ -34,12 +34,13 @@ contract Ballot {
 
     // main code
 
-    constructor(string _name, uint256 _totalSupply, bytes32[] _names, bytes32[] _descs) public payable {
+    constructor(string _name, uint256 _totalSupply, bytes8[] _names, bytes32[] _descs) public payable {
         require(_names.length > 1);
         require(_names.length == _descs.length);
         name = _name;
         author = msg.sender;
         totalSupply = _totalSupply;
+        voters[msg.sender].ticket = _totalSupply;
         for (uint8 i = 0; i < _names.length; i++) {
             proposals.push(Proposal({
                 vote : i + 1,
@@ -49,36 +50,34 @@ contract Ballot {
         }
     }
 
-    function poll(uint8 _vote) public returns (bool success) {
-        require(state);
+    function poll(uint8 _vote) public {
+        require(status);
         require(totalVotes < totalSupply);
-        require(_vote > 0, "Vote should be better than zero.");
-        require(_vote <= proposals.length, "Vote should be lower than proposals length.");
+        require(_vote > 0 && _vote <= proposals.length);
         Voter storage voter = voters[msg.sender];
-        require(!voter.voted, "Already voted");
+        require(voter.ticket > 0);
 
-        voter.voted = true;
-        voter.vote = _vote;
-        proposals[_vote - 1].supporters++;
         totalVotes++;
+        voter.ticket--;
+        voter.votes[_vote]++;
         voters[msg.sender] = voter;
+        proposals[_vote - 1].supporters++;
+
         emit Poll(msg.sender, _vote);
-        if (totalVotes == totalSupply) {
-            closed();
-        }
-        return true;
     }
 
     function opened() public {
-        state = true;
+        require(!status);
+        status = true;
         startTime = block.timestamp;
         emit Opened(startTime);
     }
 
     function closed() public payable {
-        state = false;
+        status = false;
         endTime = block.timestamp;
         emit Closed(endTime);
         selfdestruct(author);
     }
+
 }

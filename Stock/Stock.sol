099a6d8253f55dac02783876d33bcdfd6387501a
ldcc <ldcc@live.com>
2018-07-23 14:19:02 +0800

    close pay dividend

--- Stock/Stock.sol
+++ Stock/Stock.sol
@@ -14,14 +14,17 @@ contract Stock is StockInterface {
         name = _name;
         symbol = _symbol;
         founder = msg.sender;
-        holders[msg.sender].amount = _initialAmount;
-        holders[msg.sender].frees = _initialAmount;
+        holderList.push(this);
+        holderList.push(msg.sender);
+        holderMap[msg.sender].active = true;
+        holderMap[msg.sender].amount = _initialAmount;
+        holderMap[msg.sender].frees = _initialAmount;
         licensees[msg.sender][0] = true;
         licensees[msg.sender][1] = true;
     }
 
     function shareOf(address _owner, uint8 _type) external view returns (uint256 share) {
-        Holder memory h = holders[_owner];
+        Holder memory h = holderMap[_owner];
         if (_type == 1) {
             return h.amount;
         } else if (_type == 2) {
@@ -35,8 +38,8 @@ contract Stock is StockInterface {
         return allowed[_owner][_spender];
     }
 
-    function licenseOf(address _licensee, uint8 _code) external view returns (bool licensed) {
-        return licensees[_licensee][_code];
+    function licenseOf(address _licensee, address _currency) external view returns (bool licensed) {
+        return licensees[_licensee][_currency];
     }
 
     function approve(address _spender, uint256 _value) public {
@@ -44,15 +47,15 @@ contract Stock is StockInterface {
         emit Approval(msg.sender, _spender, _value);
     }
 
-    function licensing(address _licensee, uint8 _code, bool _value) public {
+    function licensing(address _licensee, address _currency, bool _value) public {
         if (_value) {
             require(msg.sender == founder);
         } else {
             require(msg.sender == _licensee);
-            require(licensees[_licensee][_code]);
+            require(licensees[_licensee][_currency]);
         }
-        licensees[_licensee][_code] = _value;
-        emit Licensing(msg.sender, _licensee, _code, _value);
+        licensees[_licensee][_currency] = _value;
+        emit Licensing(msg.sender, _licensee, _currency, _value);
     }
 
     function transfer(address _to, uint256 _value, uint256 _lockPeriod) public {
@@ -75,34 +78,56 @@ contract Stock is StockInterface {
         }
     }
 
-    function withdraw(address _to, uint8 _type, uint256 _value) public payable {
-        require(licensees[msg.sender][_type]);
-        if (_type == 0) {
+    function withdraw(address _to, address _currency, uint256 _value) public payable {
+        require(licensees[msg.sender][_currency]);
+        _withdraw(_to, _currency, _value);
+        emit Withdraw(msg.sender, _to, _currency, _value);
+    }
+
+    function payDividend(address _currency) public payable {
+        require(msg.sender == founder);
+        uint256 thisBalance = address(this).balance;
+        for (uint256 i = 0; i < holderList.length; i++) {
+            address addr = holderList[i];
+            if (holderMap[addr].amount > 0) {
+                uint8 percent = uint8(holderMap[addr].amount * 100 / totalSupply);
+                _withdraw(addr, _currency, percent * thisBalance);
+            }
+
+        }
+        emit PayDividend(msg.sender, _currency);
+    }
+
+    function _withdraw(address _to, address _currency, uint256 _value) internal {
+        if (_currency == address(0)) {
             require(_value > 0);
             assert(address(this).balance >= _value);
             _to.transfer(_value);
-        } else if(_type == 1) {
-            _transfer(this, _to, _value, 0);
+        } else if (_currency == address(this)) {
+            _transfer(_currency, _to, _value, 0);
+        } else {
+            assembly {}
+            assert(_currency.call(bytes4(keccak256("transfer")), this, _to, _value));
         }
-        emit Withdraw(msg.sender, _to, _type, _value);
-    }
-
-    function payDividend(uint8 _code) public payable {
-        _code;
-        emit PayDividend(msg.sender, _code);
     }
 
     function _transfer(address _from, address _to, uint256 _value, uint256 _lockPeriod) private {
         require(_value > 0);
-        Holder storage hf = holders[_from];
+        Holder storage hf = holderMap[_from];
+        require(hf.active);
         require(hf.amount >= _value);
         if (hf.frees < _value) {
             _upgradeHolder(hf);
         }
         require(hf.frees >= _value);
-        Holder storage ht = holders[_to];
+        Holder storage ht = holderMap[_to];
         uint256 oldHtAmount = ht.amount;
+        if (!ht.active) {
+            holderList.push(_to);
+            ht.active = true;
+        }
 
+        // transfer
         hf.amount -= _value;
         hf.frees -= _value;
         ht.amount += _value;
--- Stock/StockInterface.sol
+++ Stock/StockInterface.sol
@@ -5,6 +5,7 @@ pragma experimental "v0.5.0";
 contract StockInterface {
 
     struct Holder {
+        bool active;
         uint256 amount;
         uint256 frees;
         Share[] shares;
@@ -15,23 +16,24 @@ contract StockInterface {
         uint256 liftedPeriod;
     }
 
-    mapping(address => Holder) internal holders;
     mapping(address => mapping(address => uint256)) internal allowed;
-    // uint8 stored the currency type contract may have, 0 rep IRC, 1 rep self stock
-    mapping(address => mapping(uint8 => bool)) internal licensees;
+    // address stores the currency contract may have, "0x" rep IRC, `this` rep self stock
+    mapping(address => mapping(address => bool)) internal licensees;
+    mapping(address => Holder) internal holderMap;
 
     uint256 public totalSupply;
+    address[] public holderList;
 
     // @_type 1 rep all the holding amount, 2 rep frees holding, 3 rep all the shares in locking period
     function shareOf(address _owner, uint8 _type) external view returns (uint256 share);
 
     function allowanceOf(address _owner, address _spender) external view returns (uint256 remaining);
 
-    function licenseOf(address _licensee, uint8 _code) external view returns (bool licensed);
+    function licenseOf(address _licensee, address _currency) external view returns (bool licensed);
 
     function approve(address _spender, uint256 _value) public;
 
-    function licensing(address _licensee, uint8 _code, bool _value) public;
+    function licensing(address _licensee, address _currency, bool _value) public;
 
     function transfer(address _to, uint256 _value, uint256 _lockPeriod) public;
 
@@ -39,14 +41,14 @@ contract StockInterface {
 
     function mulTransfer(address[] _tos, uint256[] _values, uint256[] _lockPeriod) public;
 
-    function withdraw(address _to, uint8 _code, uint256 _value) public payable;
+    function withdraw(address _to, address _currency, uint256 _value) public payable;
 
-    function payDividend(uint8 currencyCode) public payable;
+    function payDividend(address _currency) public payable;
 
     // solhint-disable-next-line no-simple-event-func-name
     event Transfer(address indexed _from, address indexed _to, uint256 _value, uint256 _lockPeriod);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    event Licensing(address indexed _licensor, address indexed _licensee, uint8 _code, bool _value);
-    event Withdraw(address indexed _executor, address indexed _to, uint8 _code, uint256 _value);
-    event PayDividend(address indexed _executor, uint8 currencyCode);
+    event Licensing(address indexed _licensor, address indexed _licensee, address indexed _currency, bool _value);
+    event Withdraw(address indexed _executor, address indexed _to, address indexed _currency, uint256 _value);
+    event PayDividend(address indexed _executor, address indexed _currency);
 }

db7f679d1877900eaa297f5b9b94d698c7d074a4
ldcc <ldcc@live.com>
2018-07-30 12:31:31 +0800

    lottering and distributing rewards

--- Lottery/Chromosphere.sol
+++ Lottery/Chromosphere.sol
@@ -8,24 +8,28 @@ contract chromosphere {
     uint8 private constant MAXBLUE = 16;
     uint8 private constant MAXBALL = 7;
     uint256 private constant COOLDOWN = 86400;
-    uint256 private constant BOUNSPOOL = 5000000 ether;
+    uint256 private constant BONUSPOOL = 5000000 ether;
 
     struct Gambler {
         address addr;
         bool[MAXRED] reds;
         bool[MAXBLUE] blues;
+        uint8 bonusLevel;
+        uint256 bonus;
     }
 
     uint256 public deadline;
+    uint256 public bonusSupply;
     address public founder;
     address public pool;
     string public name;
     string public symbol;
     bool private active;
 
-    // index 0 and 1 is the percent of rewards amount pre bet
-    // index 2-5 are fixed rewards pre bet
-    uint256[6] private bonusPool;
+    // bonusPool should never be modify
+    // index 0 and 1 are the percent of each win-bonus
+    // index 2-5 are fixed bonus for each bet
+    uint256[6] private bonusPool = [10, 5, 3000, 200, 10, 5];
     uint8[MAXBALL] private answer;
     Gambler[] private gamblers;
 
@@ -47,14 +51,45 @@ contract chromosphere {
         require(_deadline >= COOLDOWN + now);
         require(now >= COOLDOWN + deadline);
 
-        delete bonusPool;
+        uint256 supply;
+        if (_currency == address(0)) {
+            supply = address(this).balance;
+        } else {
+                        bytes memory data = abi.encodeWithSignature("balanceOf(address)", address(this));
+            bytes memory ptr = new bytes(32);
+            uint8 result;
+            uint256 size;
+            assembly {
+                push1 0 push1 0
+                data mload 0x20 data add
+                push1 0 push1 _currency push1 90000
+                call
+                =: result
+                0x40 mload
+                =: ptr
+                push1 returndatasize
+                =: size
+
+                push1 size push1 0 push1 ptr
+                returndatacopy
+
+                push1 result push1 ok
+                jumpi
+                push1 size push1 ptr
+                revert
+                push1 end jump
+
+                ok :
+                ptr mload
+                =: supply
+                end :
+            }
+        }
+        require(supply >= BONUSPOOL);
+
         delete answer;
         delete gamblers;
-        if (_currency == address(0)) {
-            require(address(this).balance >= BOUNSPOOL);
-        } else {
-            require(_currency.call.gas(90000)(bytes4(keccak256("transferFrom")), msg.sender, address(this), BOUNSPOOL));
-        }
+        bonusSupply = supply;
         deadline = _deadline;
         pool = _currency;
         active = true;
@@ -65,23 +100,48 @@ contract chromosphere {
         require(active);
         require(msg.sender == founder);
         require(now >= deadline);
-        uint256 number = block.number;
-        for (uint8 i = 0; i < MAXBALL; i++) {
+        uint256 i;
+        for (i = 0; i < MAXBALL; i++) {
             if (i < RADIX) {
-                answer[i] = _randBall(blockhash(number - i), MAXRED);
+                answer[i] = _randBall(blockhash(block.number - i), MAXRED);
             } else {
-                answer[i] = _randBall(blockhash(number - i), MAXBLUE);
+                answer[i] = _randBall(blockhash(block.number - i), MAXBLUE);
             }
         }
-        // TODO get token balance
-        uint256 bal = address(this).balance;
-        for (uint256 i = 0; i < gamblers.length; i++) {
-            Gambler memory gambler = gamblers[i];
-            (uint8 level, uint256 bonus) = _evalvel(gambler, bal);
-            if (level > 0) {
-                bonusPool[level] += bonus;
+
+        uint256[] memory superWinners = new uint256[](gamblers.length);
+        (uint256 bonusAmount, uint256 remaining) = (0, 0);
+        (uint8 tops, uint8 secs, uint8 j) = (0, 0, 0);
+        for (i = 0; i < gamblers.length; i++) {
+            Gambler storage gambler = gamblers[i];
+            (uint8 level, uint256 bonus) = _evalvel(gambler);
+            gambler.bonusLevel = level;
+            gambler.bonus = bonus;
+            if (level > 2) {
+                bonusAmount += bonus;
+            } else if (level > 0) {
+                superWinners[j] = i;
+                j++;
+                if (level == 1) {
+                    tops++;
+                } else {
+                    secs++;
+                }
             }
         }
+        require(bonusSupply > bonusAmount);
+        remaining = bonusSupply - bonusAmount;
+        uint256 topBonus = remaining * 7 / 10;
+        uint256 secBonus = remaining - topBonus;
+        for (i = 0; i < superWinners.length; i++) {
+            Gambler storage gambler = gamblers[superWinners[i]];
+            if (gambler.bonusLevel == 1) {
+                gambler.bonus *= (topBonus / tops);
+            } else {
+                gambler.bonus *= (secBonus / secs);
+            }
+        }
+
         deadline = now;
         active = false;
         assert(answer.length == MAXBALL);
@@ -96,7 +156,8 @@ contract chromosphere {
         if (pool == address(0)) {
             require(msg.value >= stake);
         } else {
-            require(pool.call.gas(90000)(bytes4(keccak256("transferFrom")), msg.sender, address(this), stake));
+            bytes4 signature = bytes4(keccak256("transferFrom(address,address,uint256)"));
+            require(pool.call.gas(90000)(signature, msg.sender, address(this), stake));
         }
 
         bool[MAXRED] memory reds;
@@ -107,35 +168,25 @@ contract chromosphere {
         for (uint8 i = 0; i <= _blues.length; i++) {
             blues[_blues[i]] = true;
         }
-        Gambler memory gambler = Gambler(msg.sender, reds, blues);
+        Gambler memory gambler = Gambler(msg.sender, reds, blues, 0, 0);
         gamblers.push(gambler);
+        bonusSupply += stake;
     }
 
     function takePrize() public returns (bool) {
         require(!active);
         require(now < deadline + COOLDOWN);
-        uint256 bal;
-        if (pool == address(0)) {
-            bal = address(this).balance;
-        } else {
-            // TODO get token balance
-            require(pool.call.value(0).gas(0)(bytes4(keccak256("balanceOf")), address(this)));
-            assembly {
-                returndatacopy(0x0, 0x0, returndatasize)
-                return(0x0, returndatasize)
-            }
-        }
         for (uint256 i = 0; i < gamblers.length; i++) {
             Gambler memory gambler = gamblers[i];
             if (gambler.addr == msg.sender) {
                 delete gamblers[i];
-                (uint8 level, uint256 bonus) = _evalvel(gambler, bal);
-                if (level == 0) {
-                    return false;
-                } else {
-                    _takePrize(gambler.addr, bonus);
-                    return true;
-                }
+                //                (uint8 level, uint256 bonus) = _evalvel(gambler);
+                //                if (level == 0) {
+                //                    return false;
+                //                } else {
+                //                    _takePrize(gambler.addr, bonus);
+                //                    return true;
+                //                }
             }
         }
         return false;
@@ -146,11 +197,12 @@ contract chromosphere {
             require(address(this).balance >= _bonus);
             _winner.transfer(_bonus);
         } else {
-            require(pool.call.gas(90000)(bytes4(keccak256("transfer")), _winner, _bonus));
+            bytes4 signature = bytes4(keccak256("transfer(address,uint256)"));
+            require(pool.call.gas(gasleft())(signature, _winner, _bonus));
         }
     }
 
-    function _evalvel(Gambler memory _gambler, uint256 bal) private constant returns (uint8, uint256) {
+    function _evalvel(Gambler storage _gambler) private constant returns (uint8 level, uint256 bonus) {
         (uint8 reds, uint8 blues) = (0, 0);
         for (uint8 i = 0; i < MAXBALL; i++) {
             if (i < RADIX) {
@@ -163,28 +215,23 @@ contract chromosphere {
                 }
             }
         }
-        return _evalBonus(reds, blues, bal);
-    }
-
-    function _evalBonus(uint8 _reds, uint8 _blues, uint256 bal) private pure returns (uint8, uint256) {
-        uint8 stake = _reds + _blues;
+        uint8 stake = reds + blues;
         if (stake == MAXBALL) {
-            return (1, bal * 7 / 10);
+            level = 1;
         } else if (stake == MAXBALL - 1) {
-            if (_blues == 0) {
-                return (2, bal * 3 / 10);
+            if (blues == 0) {
+                level = 2;
             } else {
-                return (3, stake * 3000);
+                level = 3;
             }
         } else if (stake == MAXBALL - 2) {
-            return (4, stake * 200);
+            level = 4;
         } else if (stake == MAXBALL - 3) {
-            return (5, stake * 10);
-        } else if (_blues > 0) {
-            return (6, stake * 5);
-        } else {
-            return (0, 0);
+            level = 5;
+        } else if (blues > 0) {
+            level = 6;
         }
+        bonus = level > 0 ? stake * bonusPool[level - 1] : 0;
     }
 
     function _evalStake(uint8 _reds, uint8 _blues) private pure returns (uint256) {

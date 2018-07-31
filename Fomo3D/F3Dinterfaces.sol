2c666e5433d2ccbbd575c44f9495f0c907bf66f5
ldcc <ldcc@live.com>
2018-07-31 19:19:08 +0800

    clone

---
+++ Fomo3D/F3Dinterfaces.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.24;
+
+//==============================================================================
+//  . _ _|_ _  _ |` _  _ _  _  .
+//  || | | (/_| ~|~(_|(_(/__\  .
+//==============================================================================
+interface otherFoMo3D {
+    function potSwap() external payable;
+}
+
+interface DiviesInterface {
+    function deposit() external payable;
+}
+
+interface JIincForwarderInterface {
+    function deposit() external payable returns(bool);
+    function status() external view returns(address, address, bool);
+    function startMigration(address _newCorpBank) external returns(bool);
+    function cancelMigration() external returns(bool);
+    function finishMigration() external returns(bool);
+    function setup(address _firstCorpBank) external;
+}
+
+interface PlayerBookInterface {
+    function getPlayerID(address _addr) external returns (uint256);
+    function getPlayerName(uint256 _pID) external view returns (bytes32);
+    function getPlayerLAff(uint256 _pID) external view returns (uint256);
+    function getPlayerAddr(uint256 _pID) external view returns (address);
+    function getNameFee() external view returns (uint256);
+    function registerNameXIDFromDapp(address _addr, bytes32 _name, uint256 _affCode, bool _all) external payable returns(bool, uint256);
+    function registerNameXaddrFromDapp(address _addr, bytes32 _name, address _affCode, bool _all) external payable returns(bool, uint256);
+    function registerNameXnameFromDapp(address _addr, bytes32 _name, bytes32 _affCode, bool _all) external payable returns(bool, uint256);
+}
+
+interface F3DexternalSettingsInterface {
+    function getFastGap() external returns(uint256);
+    function getLongGap() external returns(uint256);
+    function getFastExtra() external returns(uint256);
+    function getLongExtra() external returns(uint256);
+}

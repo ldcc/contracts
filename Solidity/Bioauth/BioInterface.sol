3d256b63358b52a769af5ea5e82183b6cb0d6445
ldcc <ldcc@live.com>
2019-02-21 15:21:38 +0800

    rename vars, types

--- Solidity/Bioauth/BioInterface.sol
+++ Solidity/Bioauth/BioInterface.sol
@@ -7,13 +7,13 @@ contract BioInterface {
         bool exists;
         bytes32 bioInfo;
         bytes32 privKey;
-        uint256[] facilityMacs;
+        bytes32[] fMacs;
     }
 
     struct Facility {
-        uint8 facilityType;
-        uint256 facilityId;
-        uint256 facilityMac;
+        uint8 fType;
+        uint256 fId;
+        bytes32 fMac;
         bytes32 pubKey;
         bytes32 cipher;
     }
@@ -22,13 +22,13 @@ contract BioInterface {
     event Verify(bytes32 indexed _authorized, uint indexed _timestamp);
 
     mapping(bytes32 => Privacy) internal privacies;
-    mapping(bytes32 => mapping(uint256 => Facility)) internal facilitiesOwns;
+    mapping(bytes32 => mapping(bytes32 => Facility)) internal facilitiesOwns;
 
-    function facilitiesOf(bytes32 _owner) external view returns (uint256[] memory facilityMacs);
+    function facilitiesOf(bytes32 _owner) external view returns (bytes32[] memory fMacs);
 
     function encrypt(Privacy memory _privacy, Facility memory _facility) internal pure returns (bytes32 cipher);
 
-    function bind(bytes32 _bioInfo, uint8 _facilityType, uint256 _facilityId, bytes32 _pubKey) public;
+    function bind(bytes32 _bioInfo, uint8 _fType, uint256 _fId, bytes32 _fMac, bytes32 _pubKey) public;
 
-    function verify(bytes32 _bioInfo, uint8 _facilityType, uint256 _facilityId, bytes32 _pubKey) external returns(bool success);
+    function verify(bytes32 _bioInfo, uint8 _fType, uint256 _fId, bytes32 _fMac, bytes32 _pubKey) external returns (bool success);
 }
--- Solidity/Bioauth/Bioauth.sol
+++ Solidity/Bioauth/Bioauth.sol
@@ -6,28 +6,22 @@ contract Bioauth is BioInterface {
 
     constructor() public {}
 
-    function facilitiesOf(bytes32 _owner) external view returns (uint256[] memory facilityMacs) {
-        facilityMacs = privacies[_owner].facilityMacs;
+    function facilitiesOf(bytes32 _owner) external view returns (bytes32[] memory fMacs) {
+        fMacs = privacies[_owner].fMacs;
     }
 
     function encrypt(Privacy memory _privacy, Facility memory _facility) internal pure returns (bytes32 cipher) {
-        bytes memory bioInfo = _toBytes(_privacy.bioInfo);
-        bytes memory privKey = _toBytes(_privacy.privKey);
-        bytes memory facilityType = _intToBytes(_facility.facilityType);
-        bytes memory facilityId = _intToBytes(_facility.facilityId);
-        bytes memory facilityMac = _intToBytes(_facility.facilityMac);
-        bytes memory pubKey = _toBytes(_facility.pubKey);
         cipher = keccak256(
             abi.encodePacked(
-                keccak256(bioInfo),
-                keccak256(privKey),
-                keccak256(facilityType),
-                keccak256(facilityId),
-                keccak256(facilityMac),
-                keccak256(pubKey)));
+                keccak256(_toBytes(_privacy.bioInfo)),
+                keccak256(_toBytes(_privacy.privKey)),
+                keccak256(_intToBytes(_facility.fType)),
+                keccak256(_intToBytes(_facility.fId)),
+                keccak256(_toBytes(_facility.fMac)),
+                keccak256(_toBytes(_facility.pubKey))));
     }
 
-    function bind(bytes32 _bioInfo, uint8 _facilityType, uint256 _facilityId, uint256 _facilityMac, bytes32 _pubKey) public {
+    function bind(bytes32 _bioInfo, uint8 _fType, uint256 _fId, bytes32 _fMac, bytes32 _pubKey) public {
         Privacy storage privacy = privacies[_bioInfo];
         if (privacy.exists) {
             require(privacy.bioInfo == _bioInfo);
@@ -37,24 +31,24 @@ contract Bioauth is BioInterface {
             privacy.bioInfo = _bioInfo;
             privacy.privKey = '??';
         }
-        privacy.facilityMacs.push(_facilityMac);
-        Facility memory facility = Facility(_facilityType, _facilityId, _facilityMac, _pubKey, '');
+        privacy.fMacs.push(_fMac);
+        Facility memory facility = Facility(_fType, _fId, _fMac, _pubKey, '');
         bytes32 cipher = encrypt(privacy, facility);
         facility.cipher = cipher;
-        facilitiesOwns[_bioInfo][_facilityMac] = facility;
+        facilitiesOwns[_bioInfo][_fMac] = facility;
         assert(encrypt(privacy, facility) == cipher);
         emit Bind(privacy.bioInfo, now);
     }
 
-    function verify(bytes32 _bioInfo, uint8 _facilityType, uint256 _facilityId, uint256 _facilityMac, bytes32 _pubKey) external returns (bool success) {
+    function verify(bytes32 _bioInfo, uint8 _fType, uint256 _fId, bytes32 _fMac, bytes32 _pubKey) external returns (bool success) {
         Privacy memory privacy = privacies[_bioInfo];
         require(privacy.exists);
         require(privacy.bioInfo == _bioInfo);
         require(privacy.privKey == '??');
-        Facility memory facility = facilitiesOwns[_bioInfo][_facilityMac];
+        Facility memory facility = facilitiesOwns[_bioInfo][_fMac];
         bytes32 cipher = encrypt(privacy, facility);
-        require(facility.facilityType == _facilityType);
-        require(facility.facilityId == _facilityId);
+        require(facility.fType == _fType);
+        require(facility.fId == _fId);
         require(facility.pubKey == _pubKey);
         require(facility.cipher == cipher);
         success = facility.cipher == cipher;
